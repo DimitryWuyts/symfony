865479bf1d5fd581abdb7cbd969406dd474da9bb
dimitry <dimiw1337@hotmail.com>
2020-03-03 05:49:28 -0500

    lorem ipsum displaying

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\AboutMe;
+
+class LearningController extends AbstractController
+{
+    /**
+     * @Route("/learning", name="learning")
+     */
+    public function index()
+    {
+        $user = new AboutMe();
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => $user->getName(),
+            'controller_text' => $user->getText()
+        ]);
+    }
+}
